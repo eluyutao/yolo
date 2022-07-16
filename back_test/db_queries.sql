2bebc2e4b97ee57654e4206c36547731f405d4b3
Lucas <eluyutao@gmail.com>
2022-07-16 13:04:50 -0400

    add commonly used queries for future reference

---
+++ back_test/db_queries.sql
@@ -0,0 +1,23 @@
+create table if not exists stock_price_minute(
+    id integer primary key,
+    symbol text not null,
+    datetime not null,
+    open not null,
+    high not null,
+    low not null,
+    close not null,
+    volume not null,
+    trxn integer
+    -- foreign key (stock_id) references stock (id)
+)
+
+
+CREATE unique INDEX idx_stock_minute 
+ON stock_price_minute (symbol, datetime);
+
+select datetime, open, high, low, close, volume
+from stock_price_minute
+where symbol = 'AAPL'
+and strftime('%H:%M:%S', datetime) >= '09:30:00' 
+and strftime('%H:%M:%S', datetime) < '16:00:00'
+order by datetime asc
