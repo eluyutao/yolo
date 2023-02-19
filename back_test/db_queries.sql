bfb3d3346a367106b1875980fe8b93c9edf380b3
Lucas <eluyutao@gmail.com>
2023-02-19 01:28:04 -0500

    update data download script

--- back_test/db_queries.sql
+++ back_test/db_queries.sql
@@ -11,9 +11,25 @@ create table if not exists stock_price_minute(
     -- foreign key (stock_id) references stock (id)
 )
 
+create table if not exists stock_price_minute_optionable(
+    id integer primary key,
+    symbol text not null,
+    datetime not null,
+    open not null,
+    high not null,
+    low not null,
+    close not null,
+    volume not null,
+    trxn integer
+    -- foreign key (stock_id) references stock (id)
+)
+
 
 CREATE unique INDEX idx_stock_minute 
 ON stock_price_minute (symbol, datetime);
+
+CREATE unique INDEX idx_stock_minute_optionable 
+ON stock_price_minute_optionable (symbol, datetime);
 
 select datetime, open, high, low, close, volume
 from stock_price_minute
@@ -21,3 +37,6 @@ where symbol = 'AAPL'
 and strftime('%H:%M:%S', datetime) >= '09:30:00' 
 and strftime('%H:%M:%S', datetime) < '16:00:00'
 order by datetime asc
+
+
+delete from stock_price_minute_optionable
