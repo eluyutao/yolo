3ffa9de914291e9e487d1b674d7f0c8b8bbecbea
eluyutao <eluyutao@gmail.com>
2023-05-07 12:27:08 -0400

    fixed get_daily_stock_list(), previously it is using one day prior, which is wrong

--- airflow_docker/dags/dag_prep_data_orig.py
+++ airflow_docker/dags/dag_prep_data_orig.py
@@ -21,7 +21,7 @@ except:
     from utils import insert_do_nothing_on_conflicts
     from list_tickers import get_ticker_for_day, get_ticker_price
 
-
+DB_CONN_ID = 'postgres_win_remote'
 
 default_args = {
     'owner': 'Lucas',
@@ -32,20 +32,27 @@ default_args = {
 
 def get_ts():
     '''
-    ds is the logical date (or execution date), which is the interval start time + interval
-    e.g. if interval is 04-02 00:00:00 to 04-02 23:59:59, then logical date is 04-03 00:00:00 to ensure capture all the data in 04-02
-    foramt of ts: 2023-04-25T02:00:00+00:00
+    if dag start_date = pendulum.datetime(2020, 1, 1, tz="US/Eastern"), and schedule_interval='0 22 * * Mon-Fri'
+    then 
+        Data interval start = 2020-01-01, 22:00:00 EST
+        Data interval end = 2020-01-02, 22:00:00 EST,
+        utc_time: 2020-01-02T03:00:00+00:00
+        est_time: 2020-01-01T22:00:00-05:00
     '''
     utc_time = get_current_context()['ts']
     # est_time = pendulum.from_format(utc_time, 'YYYY-MM-DDTHH:MM:SS', tz='US/Eastern')
     est_time = pendulum.parse(utc_time).in_tz('US/Eastern')
+
+    # print('------------------- get_ts() function - ', f'utc_time: {utc_time}, est_time: {est_time}')
     return str(est_time)
 
 @task()
 def get_daily_stock_list():
-    ts = get_ts()
-    dt = ds_add(ts[:10],-1)
-    print(f"get_daily_stock is running!!!!!!!!!! logical timestamp is " + ts)
+    ts = get_ts() 
+    # dt = ds_add(ts[:10],-1) # 01-01
+    dt = ts[:10] # 01-01
+
+    print(f" ---------------- get_daily_stock() is running for {dt}")
     return get_ticker_for_day(dt)
 
 @task(
@@ -62,10 +69,13 @@ def start_download(stock_list, dim='day'):
     start_time = ts[:10]
     end_time = ds_add(ts[:10], 1)
 
-    postgres_hook = PostgresHook(postgres_conn_id="postgres_localhost", schema='datadl')
-    read_sql_df = postgres_hook.get_pandas_df(sql=f'''select exists(select 1
-                                                    from {tb_nm} 
-                                                    where date_time = '{start_time}') as exists
+    postgres_hook = PostgresHook(postgres_conn_id=DB_CONN_ID, schema='datadl')
+    read_sql_df = postgres_hook.get_pandas_df(sql=f'''select exists(
+                                                        select 1
+                                                        from {tb_nm} 
+                                                        where date(date_time) = to_date('{start_time}', 'YYYY-MM-DD')
+                                                        and ticker in ('TSLA', 'GOOG', 'SPY')
+                                                    ) as exists
                                                 ''')
 
     # print('read_sql_df.iloc[0,0] is : ', read_sql_df.iloc[0,0], 'with type ', type(read_sql_df.iloc[0,0]))
@@ -86,7 +96,7 @@ def start_download(stock_list, dim='day'):
 
     # if no data is returned for all tickers on that day (due to holiday), then don't do anything
     if len(df_all) > 0:
-        postgres_hook = PostgresHook(postgres_conn_id="postgres_localhost", schema='datadl')
+        postgres_hook = PostgresHook(postgres_conn_id=DB_CONN_ID, schema='datadl')
         print("---------------- start writing table to postegresql database ----------------")
         df_all.to_sql(tb_nm, postgres_hook.get_sqlalchemy_engine(), 
                     if_exists='append', index=False, chunksize=5000,
@@ -98,14 +108,14 @@ def start_download(stock_list, dim='day'):
 
 @dag(dag_id='day_stock_downloader_v0002', 
      default_args=default_args, 
-     start_date=pendulum.datetime(2022, 12, 27, tz="US/Eastern"), 
+     start_date=pendulum.datetime(2020, 1, 1, tz="US/Eastern"), 
      schedule_interval='0 22 * * Mon-Fri',
      concurrency=16,
      max_active_runs=16)
 def day_price_dl():
     create_daily_postgres_table = PostgresOperator(
             task_id='create_daily_postgres_table',
-            postgres_conn_id='postgres_localhost',
+            postgres_conn_id=DB_CONN_ID,
             sql="""
                 create table if not exists stock_price_daily (
                     ticker character varying(10) not null,
@@ -129,16 +139,16 @@ def day_price_dl():
   
     stock_list >> create_daily_postgres_table >> dl
 
-@dag(dag_id='minute_stock_downloader_v0003', 
+@dag(dag_id='minute_stock_downloader_v0002', 
      default_args=default_args, 
-     start_date=pendulum.datetime(2023, 1, 1, tz="US/Eastern"), 
+     start_date=pendulum.datetime(2020, 1, 1, tz="US/Eastern"), 
      schedule_interval='0 22 * * Mon-Fri',
-     concurrency=16,
-     max_active_runs=16)
+     concurrency=24,
+     max_active_runs=24)
 def minute_price_dl():
     create_minute_postgres_table = PostgresOperator(
             task_id='create_minute_postgres_table',
-            postgres_conn_id='postgres_localhost',
+            postgres_conn_id=DB_CONN_ID,
             sql="""
                 create table if not exists stock_price_minute (
                     ticker character varying(10) not null,
